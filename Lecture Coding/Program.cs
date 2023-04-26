d96d3f27fd266de844f7f6a3f8fde39c7db680e4
iRobSch <robin.schenkels@gmail.com>
2023-04-26 10:16:12 +0200

    Push initial files

---
+++ Lecture Coding/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Lecture_Coding;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args == null) throw new ArgumentNullException(nameof(args));
+    }
+}
