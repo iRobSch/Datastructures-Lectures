85aae7897214dce1a440329b8cea328a5bffe2d6
iRobSch <robin.schenkels@gmail.com>
2023-04-26 10:16:45 +0200

    Add functions for Binary Search

---
+++ Lecture Coding/BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿namespace Lecture_Coding;
+
+public class BinarySearch
+{
+    /// <summary>
+    /// Function discussed during the first lecture.
+    /// </summary>
+    public int Search(int[] A, int n)
+    {
+        int i = -1;
+        int j = A.Length;
+
+        while (i < j - 1)
+        {
+            int m = (i + j) / 2;
+
+            if (A[m] < n) i = m;
+            else j = m;
+        }
+
+        return j;
+    }
+
+    /// <summary>
+    /// Function written myself that resolves issues with the "Search" function.
+    /// </summary>
+    public string BSearch(int[] array, int num)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int avg = (left + right) / 2;
+
+            if (array[avg] == num) return $"The index of the value is {avg}.";
+            if (num < array[avg]) right = avg - 1;
+            else left = avg + 1;
+        }
+        return "That element does not occur within the given array.";
+    }
+
+}
--- Lecture Coding/Program.cs
+++ Lecture Coding/Program.cs
@@ -2,8 +2,13 @@
 
 public class Program
 {
+    private static readonly int[] Array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
     public static void Main(string[] args)
     {
         if (args == null) throw new ArgumentNullException(nameof(args));
+
+        Console.WriteLine(new BinarySearch().Search(Array, 15));
+        Console.WriteLine(new BinarySearch().BSearch(Array, 11));
     }
 }
