7c97781c46dd56d0095a78d210f845ccb08ba150
iRobSch <robin.schenkels@gmail.com>
2023-06-09 10:43:29 +0200

    Add HeapSort

---
+++ Lecture Coding/HeapSort.cs
@@ -0,0 +1,94 @@
+﻿namespace Lecture_Coding;
+
+public class HeapSort
+{
+    public int[] Sort(int[] array, int size)
+    {
+        if (size <= 1) return array;
+
+        BuildHeap(array, size);
+        
+
+        for (int j = size - 1; j >= 0; j--)
+        {
+            (array[0], array[j]) = (array[j], array[0]);
+            Heapify(array, j, 0);
+        }
+
+        return array;
+    }
+
+    public void Heapify(int[] array, int size, int index)
+    {
+        int largestIndex = index;
+
+        if (Left(index) < size && array[Left(index)] > array[largestIndex])
+            largestIndex = Left(index);
+
+        if (Right(index) < size && array[Right(index)] > array[largestIndex])
+            largestIndex = Right(index);
+
+        if (largestIndex != index)
+        {
+            (array[index], array[largestIndex]) = (array[largestIndex], array[index]);
+            Heapify(array, size, largestIndex);
+        }
+    }
+
+    public void BuildHeap(int[] array, int size)
+    {
+        for (int i = size / 2 - 1; i >= 0; i--)
+            Heapify(array, size, i);
+    }
+
+    public int Left(int i) => i << 1;
+    public int Right(int i) => (i << 1) | 1;
+    public int Parent(int i) => (i >> 1);
+
+    public void Rootify(int[] array, int index)
+    {
+        // Controleer of de child daarwerkelijk kleiner is.
+        if (index == 1 || array[index] > array[Parent(index)])
+            return;
+
+        // Wissel de waarden om, en pas Rootify recursief toe.
+        (array[index], array[Parent(index)]) = (array[Parent(index)], array[index]);
+        Rootify(array, Parent(index));
+    }
+
+    public int ExtractMin(int[] array, int size)
+    {
+        int result = array[1];
+
+        // Zet de laatste key op de plaats van de root.
+        array[1] = array[size--];
+
+        // Corrigeer de heap d.m.v. Heapify.
+        Heapify(array, size, 1);
+
+        return result;
+    }
+
+    public void Insert(int[] array, int size, int key)
+    {
+        // Voeg de key toe op de lege eindpositie.
+        array[size] = key;
+
+        // Corrigeer de heap d.m.v. Rootify.
+        Rootify(array, size);
+    }
+
+    /// <summary>
+    /// Method to deprioritize a key in Dijkstra's algorithm.
+    /// </summary>
+    public void DecreaseKey(int[] array, int index, int key)
+    {
+        // A[i] > k moet gelden.
+        if (array[index] < key)
+            return;
+
+        array[index] = key;
+        // De heap moet worden gecorrigeerd.
+        Rootify(array, index);
+    }
+}
--- Lecture Coding/Program.cs
+++ Lecture Coding/Program.cs
@@ -4,24 +4,27 @@ public class Program
 {
     private static readonly int[] SortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     private static readonly int[] UnsortedArray = { 5, 10, 3, 18, 2, 5, 7, 12 };
-    private static readonly char[] UnsortedCharArray = { 'a', 'c', '8', 'z', 'f', 'z', '6', 'p', '8' };
+    private static readonly int[] UnsortedHeapArray = { 73, 57, 49, 99, 133, 20, 1 };
 
     public static void Main(string[] args)
     {
         if (args == null) throw new ArgumentNullException(nameof(args));
 
         // Binary Search
-        Console.WriteLine(new BinarySearch().Search(SortedArray, 15));
-        Console.WriteLine(new BinarySearch().BSearch(SortedArray, 11));
+        // Console.WriteLine(new BinarySearch().Search(SortedArray, 15));
+        // Console.WriteLine(new BinarySearch().BSearch(SortedArray, 11));
 
         // Quick Sort
-        Console.WriteLine("Insertion sort: [{0}]", string.Join(", ", new Sorting().InsertionSort(UnsortedArray)));
-        Console.WriteLine("Selection sort: [{0}]", string.Join(", ", new Sorting().SelectionSort(UnsortedArray)));
-        Console.WriteLine("Quick sort: [{0}]", string.Join(", ", new Sorting().QuickSort(UnsortedArray, 1, UnsortedArray.Length)));
+        // Console.WriteLine("Insertion sort: [{0}]", string.Join(", ", new Sorting().InsertionSort(UnsortedArray)));
+        // Console.WriteLine("Selection sort: [{0}]", string.Join(", ", new Sorting().SelectionSort(UnsortedArray)));
+        // Console.WriteLine("Quick sort: [{0}]", string.Join(", ", new Sorting().QuickSort(UnsortedArray, 1, UnsortedArray.Length)));
 
         // Linear Sort
-        Console.WriteLine("Counting sort: [{0}]", string.Join(", ", new LinearSort().CountingSort(UnsortedArray)));
-        Console.WriteLine("Radix counting sort: [{0}]", string.Join(", ", new LinearSort().RadixSort(UnsortedArray)));
-        Console.WriteLine("Bucket sort: [{0}]", string.Join(", ", new LinearSort().BucketSort(UnsortedArray)));
+        // Console.WriteLine("Counting sort: [{0}]", string.Join(", ", new LinearSort().CountingSort(UnsortedArray)));
+        // Console.WriteLine("Radix counting sort: [{0}]", string.Join(", ", new LinearSort().RadixSort(UnsortedArray)));
+        // Console.WriteLine("Bucket sort: [{0}]", string.Join(", ", new LinearSort().BucketSort(UnsortedArray)));
+
+        // Heap Sort
+        Console.WriteLine("Heap sort: [{0}]", string.Join(", ", new HeapSort().Sort(UnsortedHeapArray, UnsortedHeapArray.Length)));
     }
 }
